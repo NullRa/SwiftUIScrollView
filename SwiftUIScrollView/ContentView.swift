fecf2c569e63b3af306cf9c08461bb5f6e8e77f7
jyang <jTang@walkgame.com>
2021-11-04 17:09:43 +0800

    加入置頂的欄位,若不要置頂,則包入ScrollView之中即可

--- SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/ContentView.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                CardView(image: "1", category: "Picture 1", heading: "Fat Cat", author: "take by null")
-                CardView(image: "2", category: "Picture 2", heading: "LOVE", author: "take by null")
-                CardView(image: "3", category: "Picture 3", heading: "Dude", author: "take by null")
-                CardView(image: "4", category: "Picture 4", heading: "Long Leg", author: "take by null")
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("THURSDAY,NOV,4")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text("Your Photos")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                }
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            ScrollView {
+                VStack {
+                    CardView(image: "1", category: "Picture 1", heading: "Fat Cat", author: "take by null")
+                    CardView(image: "2", category: "Picture 2", heading: "LOVE", author: "take by null")
+                    CardView(image: "3", category: "Picture 3", heading: "Dude", author: "take by null")
+                    CardView(image: "4", category: "Picture 4", heading: "Long Leg", author: "take by null")
+                }
             }
         }
     }
