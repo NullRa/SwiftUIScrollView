647d616523938c37b2f55ba0c1df98d50ed62446
jyang <jTang@walkgame.com>
2021-11-04 17:04:02 +0800

    將內容包進ScrollView之中讓畫面可滑動

--- SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            CardView(image: "1", category: "Picture 1", heading: "Fat Cat", author: "take by null")
-            CardView(image: "2", category: "Picture 2", heading: "LOVE", author: "take by null")
-            CardView(image: "3", category: "Picture 3", heading: "Dude", author: "take by null")
-            CardView(image: "4", category: "Picture 4", heading: "Long Leg", author: "take by null")
+        ScrollView {
+            VStack {
+                CardView(image: "1", category: "Picture 1", heading: "Fat Cat", author: "take by null")
+                CardView(image: "2", category: "Picture 2", heading: "LOVE", author: "take by null")
+                CardView(image: "3", category: "Picture 3", heading: "Dude", author: "take by null")
+                CardView(image: "4", category: "Picture 4", heading: "Long Leg", author: "take by null")
+            }
         }
     }
 }
