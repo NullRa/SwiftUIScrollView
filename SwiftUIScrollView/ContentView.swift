056a905b6aceb720b55e0aafe70aaeee80a22333
jyang <jTang@walkgame.com>
2021-11-04 17:18:06 +0800

    使用群組視圖簡化程式碼,可以將修飾器加入群組,會對群組內所有視圖同步產生效果

--- SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/ContentView.swift
@@ -26,14 +26,13 @@ struct ContentView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
-                    CardView(image: "1", category: "Picture 1", heading: "Fat Cat", author: "take by null")
-                        .frame(width:UIScreen.main.bounds.width)
-                    CardView(image: "2", category: "Picture 2", heading: "LOVE", author: "take by null")
-                        .frame(width:UIScreen.main.bounds.width)
-                    CardView(image: "3", category: "Picture 3", heading: "Dude", author: "take by null")
-                        .frame(width:UIScreen.main.bounds.width)
-                    CardView(image: "4", category: "Picture 4", heading: "Long Leg", author: "take by null")
-                        .frame(width:UIScreen.main.bounds.width)
+                    Group {
+                        CardView(image: "1", category: "Picture 1", heading: "Fat Cat", author: "take by null")
+                        CardView(image: "2", category: "Picture 2", heading: "LOVE", author: "take by null")
+                        CardView(image: "3", category: "Picture 3", heading: "Dude", author: "take by null")
+                        CardView(image: "4", category: "Picture 4", heading: "Long Leg", author: "take by null")
+                    }
+                    .frame(width:UIScreen.main.bounds.width)
                 }
             }
         }
