6c3a5c96a610a89c7ba9fbb5a326a00c96affe7c
jyang <jTang@walkgame.com>
2021-11-04 16:48:29 +0800

    加入一些程式碼把卡片的樣子做出來

--- SwiftUIScrollView/CardView.swift
+++ SwiftUIScrollView/CardView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Picture 1")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text("Fat Cat")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text("take by null".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
+        .cornerRadius(30)
     }
 }
 
